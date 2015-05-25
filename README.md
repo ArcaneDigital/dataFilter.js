954a128f8a0e721e1df4a92c8860593bb4fdbf2a
Jay Goodfellow <j.goodfellow@arcane.ws>
2015-05-25 10:57:41 -0400

    added extra methods and separated the code a bit

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # dataFilter.js
 Filter and sort elements using data attributes
+
+## Examples
+    dataFilter.init({
+         filterclass: 'filter',
+         dataParent: 'dataParent',
+         sortAttr: 'data-sort'
+    });
+
+## Documentation
+
+`dataFilter.init(options)`
+
+### Options
+- `filterClass` the class used on buttons or links that are used to filter the data
+- `dataParent` the ID of the parent element which contains the data to filter
+- `sortAttr` the data attribute that is used to sort the items
--- dataFilter.js
+++ dataFilter.js
@@ -13,16 +13,22 @@ var dataFilter = {
 			el.addEventListener('click', dataFilter.eventFilter, false);
 		});
 		dataFilter.options = options;
-		document.getElementById(options.dataCount).innerHTML = parent.children.length;
-
+		dataFilter.filterElements();
+		dataFilter.sortElements();   
+		dataFilter.setElements();   
 	},
 
 	eventFilter: function(){
-		var options = dataFilter.options;
 		var attr = this.getAttribute('data-attr');
-		var value = this.getAttribute('data-value');
+		var value = this.getAttribute('data-value');		
+		dataFilter.filterElements(attr, value);
+		dataFilter.sortElements();
+		dataFilter.setElements();
+	},
+	filterElements: function(attr, value){
+		var options = dataFilter.options;
 		var parent = options.parent;
-		if(window.filterView == undefined){window.filterView = new Array();}
+		if(window.filterView == undefined){window.filterView = Array.prototype.slice.call(options.parent.children);}
 		if(window.filterCache == undefined){window.filterCache = new Array();}
 		if(filterCache.length + filterView.length > 0) {
 		    var items = filterView.concat(filterCache);
@@ -40,13 +46,19 @@ var dataFilter = {
 		        filterCache.push(el);
 		    }           
 		});
-		filterView.sort(function sortfunction(a, b){
-		    return (b.getAttribute(options.sortAttr) - a.getAttribute(options.sortAttr) );
-		});
-		parent.innerHTML = '';
+	},
+	setElements: function() {
+		var parent = dataFilter.options.parent;
+		while (parent.firstChild) {
+		  parent.removeChild(parent.firstChild);
+		}
 		for(var i=0;i<filterView.length;i++){
 		    parent.appendChild(filterView[i]);
-		} 	
-		document.getElementById(options.dataCount).innerHTML = parent.children.length;	
+		} 
+	},
+	sortElements: function(){
+		window.filterView.sort(function sortfunction(a, b){
+		    return (b.getAttribute(dataFilter.options.sortAttr) - a.getAttribute(dataFilter.options.sortAttr) );
+		});		
 	}
 };
